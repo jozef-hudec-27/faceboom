126b3514b6ebbfb982199f493c9291660026d030
Jozef Hudec <jojohudec27@gmail.com>
2022-11-04 19:36:21 +0100

    Add associations between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable
-    validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, presence: true
+
+  has_many :sent_friend_requests, class_name: 'FriendRequest', foreign_key: 'sender_id'
+  has_many :received_friend_requests, class_name: 'FriendRequest', foreign_key: 'receiver_id'
+  has_many :posts
+  has_many :likes
+  has_many :comments
+  has_and_belongs_to_many :friends, class_name: 'User',
+                                    join_table: :friendships,
+                                    foreign_key: :user_id,
+                                    association_foreign_key: :friend_user_id 
 end
