a1f76220af6e7076040e78d5fbd1423dcbe5e703
Jozef Hudec <jojohudec27@gmail.com>
2022-11-13 19:58:31 +0100

    Send welcome email when user confirms email address

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -17,7 +17,18 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   end
 
   def show
-    super
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+
+      DeviseMailer.with(user: resource).welcome.deliver_later
+
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
   end
 
   protected
--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -13,4 +13,9 @@ class DeviseMailer < Devise::Mailer
     @user = record
     super
   end
+
+  def welcome
+    @user = params[:user]
+    mail to: @user.email, subject: 'Welcome to Faceboom!', template_path: 'devise/mailer', template_name: 'welcome'
+  end
 end
