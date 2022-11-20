8bb728854f1db044aeba58417002e3eb3f2a5581
Jozef Hudec <jojohudec27@gmail.com>
2022-11-20 13:18:46 +0100

    Implement scrolling to element on page load with anchor

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:load', function() {
+  let anchor = window.location.hash.slice(1)
+  
+  if (anchor) {
+    let elementToScrollTo = document.getElementById(anchor)
+
+    if (elementToScrollTo) {
+      let headerOffset = document.querySelector('.navbar').offsetHeight + 10;
+      let elementPosition = elementToScrollTo.getBoundingClientRect().top;
+      let offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+      window.scrollTo({
+          top: offsetPosition,
+      });
+    }
+  }
+})
