5b69b7e2d7d3b292eeed8081ee2eadc998b19e16
Jozef Hudec <jojohudec27@gmail.com>
2022-12-06 19:43:57 +0100

    Make modal_controller.js support multiple modals

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,13 +3,21 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ['modal']
 
-  showModal() {
-    this.modalTarget.classList.add('is-active')
+  findModal(caller) {
+    if (this.modalTargets.length == 1) {
+      return this.modalTarget
+    }
+
+    return this.modalTargets.find(m => m.id == caller.dataset.mdl || caller.parentNode.dataset.mdl)
+  }
+
+  showModal(e) {
+    this.findModal(e.target).classList.add('is-active')
     document.querySelector('html').classList.add('is-clipped')
   }
 
-  hideModal() {
-    this.modalTarget.classList.remove('is-active')
+  hideModal(e) {
+    this.findModal(e.target).classList.remove('is-active')
     document.querySelector('html').classList.remove('is-clipped')
   }
 }
