87f2744183befee096c025ed2be5baf6776deeee
Jozef Hudec <jojohudec27@gmail.com>
2023-01-06 06:10:00 +0100

    Stop tracking assets + delete needless JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import "./custom/users_search";
-import "./custom/anchor_scroll";
 import "channels";
+
+// Custom JS to make top searchbar work
+document.addEventListener('turbo:load', () => {
+  let searchFaceboomInput = document.getElementById('search-faceboom-input');
+
+  if (searchFaceboomInput) {
+    searchFaceboomInput.value = new URLSearchParams(window.location.search).get('q');
+    
+    document.addEventListener('keydown', e => {
+      if (e.code == 'Enter' && document.activeElement == searchFaceboomInput) {
+          window.location.href = '/users/?q=' + searchFaceboomInput.value;
+      };
+    });
+  };
+});
--- public/assets/controllers/dropdown_controller-d7fa8ec3e1026c49498f362c9a6891340140cf8df5e938b3b045d58f1b2f65a0.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  static targets = ['main'];
-
-  showDropdown() {
-    this.mainTarget.classList.toggle('is-active');
-  };
-};
